b66ebfc65d4439bcdfffb6c125e6ec46988d9df2
AdriA22 <adriasuarez79@gmail.com>
2023-05-24 15:13:05 -0300

    correcciones

---
+++ videos.js
@@ -0,0 +1,14 @@
+$(".video-link").click(function () {
+  var theModal = $(this).data("target");
+  videoSRC = $(this).attr("data-video");
+  videoSRCauto =
+    videoSRC + "?modestbranding=1&rel=0&showinfo=0&html5=1&autoplay=1";
+  $(theModal + " iframe").attr("src", videoSRCauto);
+  $(theModal + " button.close").click(function () {
+    $(theModal + " iframe").attr("src", videoSRC);
+  });
+  $(".close").click(() => {
+    $(".myFrame").removeAttr("src");
+  });
+});
+
